50efe5116ed673b3b2a19e2cc3626df60d16aff9
mahmoud zaky <78617936+zaky-fetoh@users.noreply.github.com>
2021-12-03 17:35:24 +0200

    kag

--- data_org/PATHS.py
+++ data_org/PATHS.py
@@ -2,5 +2,5 @@
 # NORMAL_DIR_PATH= 'E:/covdt_CoCo/DATASETS/QaTa-COV19/Control_Group/Control_Group_I/'
 #
 
-COVID_DIR_PATH = '../DATASET/QaTaCov/QaTa-COV19/QaTa-COV19/Images/'
-NORMAL_DIR_PATH= '../DATASET/QaTaCov/QaTa-COV19/Control_Group/Control_Group_I/'
+COVID_DIR_PATH = '../input/qatacov19-dataset/QaTa-COV19/QaTa-COV19/Images'
+NORMAL_DIR_PATH= '../input/qatacov19-dataset/QaTa-COV19/Control_Group/Control_Group_I'
