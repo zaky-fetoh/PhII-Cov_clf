e52b282ef9556be597e3598031b88cd658e59c2a
mahmoud zaky <78617936+zaky-fetoh@users.noreply.github.com>
2021-12-04 20:14:26 +0200

    fixedtrainProfile

---
+++ weights/fixDistrMetr.py
@@ -0,0 +1,23 @@
+from glob import glob
+import pickle
+
+def save_train_hist(hist, ep_num,
+                    name='hist',
+                    outPath='./weights/'):
+    file_name = outPath + name + str(ep_num) + '.p'
+    with open(file_name, 'wb') as file:
+        pickle.dump(hist, file)
+
+
+def fix():
+    accum = dict()
+    for file_name in glob('hist*'):
+        with open(file_name, 'rb')as file:
+            hist = pickle.load(file)
+        accum.update(hist)
+    return accum
+
+
+
+acc = fix()
+save_train_hist(acc, 23,outPath='./')
