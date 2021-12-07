a93cfd5338cc31fe9baef395f823e31f576db776
mahmoud zaky <78617936+zaky-fetoh@users.noreply.github.com>
2021-12-07 20:34:45 +0200

    V#3

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ test
 if __name__ == '__main__':
     pres = 99
     mdl.t.cuda.empty_cache()
-    mobj = tr.Network(bs=16,ustep=256,)
-    mobj.opt = tr.optim.Adam(mobj.network.parameters(),1e-5)
-    mobj.load(pres)
-    mobj.fit(epoch=31,startwith=pres+1)
+    mobj = tr.Network(bs=16,ustep=256,cfx=mdl.SameConv2d)
+    mobj.opt = tr.optim.Adam(mobj.network.parameters(),1e-3)
+    #mobj.load(pres)
+    mobj.fit(epoch=31,)#startwith=pres+1)
     #mobj.test()
